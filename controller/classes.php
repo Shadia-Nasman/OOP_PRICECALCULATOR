682dd67ec9350d8b02563437d9d76ca62a8cde30
shadia <shadia.nasman1988@gmail.com>
2019-12-16 16:46:26 +0100

    first structure

---
+++ controller/classes.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    
+    <?php
+class customer {
+    // Properties
+    public $customer_name;
+    public $customer_group;
+  
+    // Methods
+    // function set_name($name) {
+    //   $this->name = $name;
+    // }
+    // function get_name() {
+    //   return $this->name;
+    // }
+  }
+
+  class product {
+    // Properties
+    public $product_name;
+    public $product_price;
+
+  }
+
+  class group {
+    // Properties
+    public $group_name;
+    public $group_discount;
+  }
+  ?>
+        
+    </body>
+    </html>
